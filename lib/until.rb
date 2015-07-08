43e63243fb2d905d8d1b20d246857a9304fc7328
mbrickenworthe <adamwinters456@gmail.com>
2015-07-08 19:44:08 -0400

    Such an odd way of getting it...

--- lib/until.rb
+++ lib/until.rb
@@ -1,6 +1,10 @@
 def using_until
     levitation_force = 6
-    #your code here
-    
+    loopingString = ""
+    until levitation_force == 10 do
+      levitation_force += 1
+      loopingString += "Wingardium Leviosa\n"
+    end
+    puts loopingString   
 end
 
--- lib/while.rb
+++ lib/while.rb
@@ -1,7 +1,12 @@
 def using_while
-	levitation_force = 6
-	
-	#your code here
+    levitation_force = 6
+    loopingString = ""
+    while levitation_force < 10 do
+      levitation_force += 1
+     loopingString += "Wingardium Leviosa\n"
+    end
+  print loopingString    
 end
+
 
 
